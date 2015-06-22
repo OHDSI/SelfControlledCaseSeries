80a863117780cb2c87403618620620895e49b5c7
Marc Suchard <msuchard@gmail.com>
2015-06-22 13:42:42 -0700

    Sort covariate properly for new interface

--- R/ModelFitting.R
+++ R/ModelFitting.R
@@ -3,13 +3,13 @@
 # Copyright 2014 Observational Health Data Sciences and Informatics
 #
 # This file is part of SelfControlledCaseSeries
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -40,6 +40,9 @@ fitSccsModel <- function(sccsEraData,
   # If we're fitting a model specifically for one exposure, exclude that one from regularization:
   if (!is.null(exposureConceptId))
     prior$exclude <- exposureConceptId
+
+  # Re-sort covariates for new interface.  TODO move sorting into SQL
+  covariatesSubset <- covariatesSubset[ff::ffdforder(covariatesSubset[c("covariateId", "stratumId","rowId")]),]
 
   cyclopsData <- Cyclops::convertToCyclopsData(outcomesSubset,
                                                covariatesSubset,
