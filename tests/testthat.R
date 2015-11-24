c3af4ad6a5531a90e7d7c07749a8e1a85ccfc235
schuemie <schuemie@ohdsi.org>
2015-11-24 14:29:45 +0800

    Fixed unit testing during R check

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,2 +1,7 @@
+# Workaround to fix unit tests in check as described here:
+# https://github.com/hadley/testthat/issues/86
+Sys.setenv("R_TESTS" = "")
+
 library(testthat)
+library(SelfControlledCaseSeries)
 test_check("SelfControlledCaseSeries")
