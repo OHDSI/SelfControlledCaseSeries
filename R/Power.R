59e51d37ce3dc769b245b081d94416cf2009cdf5
Jamie Weaver <jamieweaver@gmail.com>
2017-01-25 10:55:22 -0500

    Added signed root likelihood ratio method, set binomial proportion method as default

--- R/Power.R
+++ R/Power.R
@@ -20,20 +20,20 @@
 #'
 #' @details
 #' Compute the minimum detectable relative risk (MDRR) for a given study population, using the observerd
-#' time at risk and total time in days and number of events. Four sample size formulas are implemented:
-#' sampling proportion, binomial proportion, signed root likelihood ratio, and likelihood extension for
+#' time at risk and total time in days and number of events. Five sample size formulas are implemented:
+#' sampling proportion, binomial proportion, 2 signed root likelihood ratio methods, and likelihood extension for
 #' age effects. The expressions by Musonda (2006) are used.
 #'
-#' @param sccsEraData  A data frame describing the study population as created using the
-#'                     \code{\link{createSccsEraData}} function. This should at least have these
-#'                     columns: subjectId, treatment, outcomeCount, timeAtRisk.
+#' @param sccsEraData  An object containing study population observation time, outcomesa and covariates as created
+#'                     using the \code{\link{createSccsEraData}} function. This should include the following dataframes:
+#'                     outcomes, covariates, and covariateRef.
 #' @param heiConceptId ConceptId for the health exposure of interest.
 #' @param alpha        Type I error.
 #' @param power        1 - beta, where beta is the type II error.
 #' @param twoSided     Consider a two-sided test?
 #' @param method       The type of sample size formula that will be used. Allowable values are
-#'                     "proportion", "binomial", "SRL", or "ageEffects". Currently only "proportion"
-#'                     is supported.
+#'                     "proportion", "binomial", "SRL1", "SRL2", or "ageEffects". Currently "ageEffects"
+#'                     is not supported.
 #'
 #' @references
 #' Musonda P, Farrington CP, Whitaker HJ (2006) Samples sizes for self-controlled case series studies,
@@ -49,26 +49,26 @@ computeMdrr <- function(sccsEraData,
                         alpha = 0.05,
                         power = 0.8,
                         twoSided = TRUE,
-                        method = "distribution") # distribution, binomial, SRL, ageEffects
+                        method = "binomial") # distribution, binomial, SRL1, SRL2, ageEffects
 {
 
   heiCovariateIds <- sccsEraData$covariateRef[ffbase::`%in%`(sccsEraData$covariateRef$originalCovariateId, heiConceptId), ]$covariateId
   mapping <- ffbase::ffmatch(ffbase::unique.ff(sccsEraData$covariates$rowId[ffbase::`%in%`(sccsEraData$covariates$covariateId, heiCovariateIds)]), sccsEraData$outcomes$rowId)
-  tExp <- ffbase::sum.ff(sccsEraData$outcomes$time[mapping]) # exposed time
-  tTot <- sum(sccsEraData$outcomes$time)                     # total time
-  r <- tExp / tTot                                           # exposed time / total time
+  timeExposed <- ffbase::sum.ff(sccsEraData$outcomes$time[mapping]) # exposed time
+  timeTotal <- sum(sccsEraData$outcomes$time)                       # total time
+  r <- timeExposed / timeTotal                                      # exposed time / total time
   if (twoSided) {
-    alpha <- alpha / 2                                       # alpha
+    alpha <- alpha / 2                                              # alpha
   }
-  z <- qnorm(1-alpha)                                        # z alpha
-  n <- ffbase::sum.ff(sccsEraData$outcomes$y)                # number of events
+  z <- qnorm(1-alpha)                                               # z alpha
+  n <- ffbase::sum.ff(sccsEraData$outcomes$y)                       # number of events
 
-  if (method != "distribution" && method != "binomial" && method != "SRL" && method != "ageEffects")
+  if (method != "distribution" && method != "binomial" && method != "SRL1" && method != "SRL2" && method != "ageEffects")
     stop(paste0("Unknown method '",
                 method,
-                "', please choose either 'distribution', 'binomial', 'SRL', or 'ageEffects'"))
+                "', please choose either 'distribution', 'binomial', 'SRL1', 'SRL2', or 'ageEffects'"))
 
-  if (method == "distribution")
+  if (method == "distribution") # expression 5
   {
     computePower <- function(p, z, r, n, alpha)
     {
@@ -82,7 +82,7 @@ computeMdrr <- function(sccsEraData,
     }
   }
 
-  if (method == "binomial")
+  if (method == "binomial") # expression 6
   {
     computePower <- function(p, z, r, n, alpha)
     {
@@ -97,9 +97,35 @@ computeMdrr <- function(sccsEraData,
     }
   }
 
-  if (method == "SRL")
+  if (method == "SRL1") # expression 7
   {
-    stop("Binomial method not currently supported")
+    computePower <- function(b, z, r, n, alpha)
+    {
+      A = 2 * ((exp(b) * r / (exp(b) * r + 1 - r)) * b - log(exp(b) * r + 1 - r))
+      B = b^2 / A * exp(b) * r * (1 - r) / (exp(b) * r + 1 - r)^2
+      zb <- (sqrt(n * A) - z) / sqrt(B)
+      power <- pnorm(zb)
+      if (power < alpha | n < 1)
+        power <- alpha
+      return(power)
+    }
+  }
+
+  if (method == "SRL2") # expression 8
+  {
+    nExposed <- length(unique(sccsEraData$outcomes$stratumId))
+    nTotal <- length(unique(sccsEraData$outcomes$stratumId[mapping]))
+    pr <- nExposed / nTotal # proportion of population exposed
+    computePower <- function(b, z, r, n, alpha)
+    {
+      A = 2 * pr * (exp(b) * r + 1 - r) / (1 + pr * r * (exp(b) - 1)) * ((exp(b) * r / (exp(b) * r + 1 - r)) * b - log(exp(b) * r + 1 - r))
+      B = b^2 / A * pr * (exp(b) * r + 1 - r) / (1 + pr * r * (exp(b) - 1)) * exp(b) * r * (1-r) / (exp(b) * r + 1 - r)^2
+      zb <- (sqrt(n * A) - z) / sqrt(B)
+      power <- pnorm(zb)
+      if (power < alpha | n < 1)
+        power <- alpha
+      return(power)
+    }
   }
 
   if (method == "ageEffects")
@@ -114,7 +140,12 @@ computeMdrr <- function(sccsEraData,
     while (H >= L)
     {
       M <- L + (H - L) / 2
-      powerM <- computePower(exp(M), z, r, n, alpha)
+      if (method %in% c("SRL1", "SRL2")) {
+        powerM <- computePower(M, z, r, n, alpha)
+      }
+      else {
+        powerM <- computePower(exp(M), z, r, n, alpha)
+      }
       d <- powerM - power
       if (d > precision){
         H <- M
@@ -130,8 +161,8 @@ computeMdrr <- function(sccsEraData,
   mdLogRr <- binarySearch(z, r, n, power, alpha)
   mdrr <- exp(mdLogRr)
 
-  result <- data.frame(exposedTime = tExp,
-                       totalTime = tTot,
+  result <- data.frame(timeExposed = timeExposed,
+                       timeTotal = timeTotal,
                        exposedProportion = r,
                        events = n,
                        mdrr = mdrr)
